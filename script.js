76e5ea8c958941d44d295099c2aabefa258b13e2
Awab Mohamed <awabmohamed123@gmail.com>
2024-01-18 09:54:44 -0500

    Added UI to RPS project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choiceArray = ["rock", "paper", "scissors"];
+let choiceArray = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){    
     let randomChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)]
@@ -19,39 +19,84 @@ function playRound(playerSelection, computerSelection) {
     }else if(playerSelection.toLowerCase() === choiceArray[2].toLowerCase() && computerSelection.toLowerCase() === choiceArray[1].toLowerCase()){
         return "You Win! Scissors beats Paper";
     }else if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        console.log("It is a Tie! the round will be re-played");
-        do {
-            let playerSelection = prompt("Enter a new choice:");
-            let computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection));
-            break;
-        } while (playerSelection.toLowerCase() === computerSelection.toLowerCase());
+        return "It's a tie!"
     }
   }
    
-  function game(){
-    let computerWins = 0;
+   
+  function game() {
     let playerWins = 0;
-
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Enter your choice:");
-        let computerSelection = getComputerChoice();
-        console.log("You chose: " + playerSelection + " and the computer chose: " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        
-        if(playRound(playerSelection, computerSelection) === "You Win! Rock beats Scissors" || playRound(playerSelection, computerSelection) === "You Win! Rock beats Scissors" || playRound(playerSelection, computerSelection) === "You Win! Scissors beats Paper"){
-            playerWins++;
-        }else{
-            computerWins++;
-        }
+    let computerWins = 0;
+  
+    const maxScore = 5; 
+  
+    const rockBtn = document.querySelector('#rock');
+    const paperBtn = document.querySelector('#paper');
+    const scissorsBtn = document.querySelector('#scissors');
+    const results = document.querySelector('.results-div');
+    const pScore = document.querySelector('#pScore');
+    const cScore = document.querySelector('#cScore');
+  
+    function updateScores() {
+      let playerScorePara = pScore.querySelector('p');
+      if (!playerScorePara) {
+        playerScorePara = document.createElement('p');
+        pScore.appendChild(playerScorePara);
+      }
+      playerScorePara.textContent = playerWins;
+  
+      let computerScorePara = cScore.querySelector('p');
+      if (!computerScorePara) {
+        computerScorePara = document.createElement('p');
+        cScore.appendChild(computerScorePara);
+      }
+      computerScorePara.textContent = computerWins;
     }
-    if(computerWins > playerWins){
-        console.log("The computer won " + computerWins + " times, while the player won " + playerWins + " times");
-        console.log("The computer is the winner!");
-    }else{
-        console.log("The player won " + playerWins + " times, while the computer won " + computerWins + " times");
-        console.log("The player is the winner!");
+  
+    function displayResults(res) {
+      let messageParagraph = results.querySelector('p');
+      if (!messageParagraph) {
+        messageParagraph = document.createElement('p');
+        results.appendChild(messageParagraph);
+      }
+      messageParagraph.textContent = "Results: " + res;
     }
+  
+    function checkGameOver() {
+      if (playerWins === maxScore || computerWins === maxScore) {
+        const winner = playerWins === maxScore ? "Player" : "Computer";
+        const gameOverMessage = `${winner} wins! Game Over!`;
+        displayResults(gameOverMessage);
+  
+        // Disable buttons after game over
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+      }
+    }
+  
+    function playGame(playerSelection) {
+      const computerSelection = getComputerChoice();
+      const res = playRound(playerSelection, computerSelection);
+  
+      if (res.includes("You Win")) {
+        playerWins++;
+      } else if (res.includes("You Lose")) {
+        computerWins++;
+      }
+  
+      displayResults(res);
+      updateScores();
+      checkGameOver();
+    }
+  
+    rockBtn.addEventListener('click', () => playGame(choiceArray[0]));
+  
+    paperBtn.addEventListener('click', () => playGame(choiceArray[1]));
+  
+    scissorsBtn.addEventListener('click', () => playGame(choiceArray[2]));
   }
-
-  console.log(game());
+  
+  game();
+  
+   
